f32e1b273bf0fb95e37bd86d279d5a0a85105f38
Markus Radspieler <markus.radspieler@bmw.de>
2023-06-28 15:08:21 +0200

    big change to ONE file

--- after/plugin/tagbar.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_set_keymap('n', '<leader>t', "<cmd>TagbarToggle<CR>", { noremap = true, silent = true })
--- lua/mradspieler/init.lua
+++
@@ -1,3 +0,0 @@
-require("mradspieler.remap")
-require("mradspieler.set")
-require("mradspieler.packer")
